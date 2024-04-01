4a674f806a821f62e7961b315da3f9ec0857ccab
Vedant Agarwal <92169206+VedantAgarwal02@users.noreply.github.com>
2024-04-01 12:44:45 +0530

    Update backendUrl.ts 1.4.23:1244

--- src/backendUrl.ts
+++ src/backendUrl.ts
@@ -3,6 +3,6 @@
 // module.exports = url;
 
 export class BackendUrl { 
-    url:string = "http://localhost:4000"
-    // url:string = "https://employee-dashboard-backend-peach.vercel.app/";
+    //url:string = "http://localhost:4000"
+    url:string = "https://employee-dashboard-backend-peach.vercel.app/";
 }
